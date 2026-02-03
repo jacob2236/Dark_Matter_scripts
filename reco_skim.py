05b9e71b6efac6d2573717cc7902ff3d67601ba4
jacob2236 <woodsjt321@gmail.com>
2026-02-03 10:23:41 -0500

    finds electrons

--- reco_skim.py
+++ reco_skim.py
@@ -1,4 +1,14 @@
 import ROOT
 
 f = ROOT.TFile.Open("reco.root")
-f.ls()  # lists contents
+#f.ls()  # lists contents
+
+tree = f.Get("events")
+
+for event in tree:
+    particles = event.ReconstructedParticles
+    for p in particles:
+        if p.PDG == 11:   # electron
+            mom = p.momentum
+            px, py, pz = mom.x, mom.y, mom.z
+            print("Electron p =", (px**2 + py**2 + pz**2)**0.5)
